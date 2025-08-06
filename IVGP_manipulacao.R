fda7ab722a52fcac8a97061608d2759e876b7f81
Pedro Henrique Heinzen <phheinzen@furb.br>
2025-08-06 13:03:08 -0300

    Criado IVGP_manipulacao
    Criado script de manipulacao dos dados de IVGP

---
+++ IVGP_manipulacao.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+# definindo caminho para diretorio
+setwd("C:\\Users\\pedro\\Documents\\git\\IVGP")
+
+# lendo arquivo IVGP
+IVGP <- read.csv(".\\arquivos\\IVGP.csv", sep = ";")
+
+# substituindo "," por "." para os decimais das colunas
+IVGP <- IVGP|>
+  map(~ gsub(",", ".", .x)) |>
+  as.data.frame()
+
+# alterando nome das colunas
+colnames(IVGP)[colnames(IVGP) == "mes_..."] <- "mes_(%)"
+colnames(IVGP)[colnames(IVGP) == "ano_..."] <- "ano_(%)"
+colnames(IVGP)[colnames(IVGP) == "X6_meses_..."] <- "em_6_meses_(%)"
+colnames(IVGP)[colnames(IVGP) == "X12_meses_..."] <- "em_12_meses_(%)"
+
+# alterando tipo das colunas
+IVGP$`mes_(%)` <- as.double(IVGP$`mes_(%)`)
+IVGP$`ano_(%)` <- as.double(IVGP$`ano_(%)`)
+IVGP$`em_6_meses_(%)` <- as.double(IVGP$`em_6_meses_(%)`)
+IVGP$`em_12_meses_(%)` <- as.double(IVGP$`em_12_meses_(%)`)
+IVGP$`numero_indice` <- as.double(IVGP$`numero_indice`)
+
+# alterando a coluna "data" para POSIXct
+IVGP$data <- as.POSIXct(IVGP$data, format = "%d/%m/%Y")
