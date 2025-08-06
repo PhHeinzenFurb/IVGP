b07c721442658c9a77abc435c73e227e4ed92362
Pedro Henrique Heinzen <phheinzen@furb.br>
2025-08-06 13:39:57 -0300

    Atualizado IVGP_manipulacao e Criado IVGP_formatado
    Finalizado processo de manipulacao dos dados do IVGP, sendo criadas as colunas para analises MoM e YoY. O resultado da manipulacao foi salva no arquiv IVGP_formatado.csv

--- IVGP_manipulacao.R
+++ IVGP_manipulacao.R
@@ -26,3 +26,34 @@ IVGP$`numero_indice` <- as.double(IVGP$`numero_indice`)
 
 # alterando a coluna "data" para POSIXct
 IVGP$data <- as.POSIXct(IVGP$data, format = "%d/%m/%Y")
+
+# fazendo backup
+backup_IVGP <- IVGP
+
+# variacao MoM para os valores mensais de IVGP 
+IVGP$`MoM_IVGP_mes(%)` <- (IVGP$`mes_(%)` - lead(IVGP$`mes_(%)`))/lead(IVGP$`mes_(%)`)
+
+# variacao YoY para os valores anuais de IVGp
+IVGP$`YoY_IVPG_ano(%)` <- (IVGP$`ano_(%)` - lead(IVGP$`ano_(%)`, n = 12)) / lead(IVGP$`ano_(%)`, n = 12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
